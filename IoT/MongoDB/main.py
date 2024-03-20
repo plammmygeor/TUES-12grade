0e7f17d805a86dab9cd841a93cf068faaf03c302
Plamena Georgieva <plamena.v.georgieva.2019@elsys-bg.org>
2024-03-20 09:45:45 +0200

    database
    mongo

--- IoT/MongoDB/main.py
+++ IoT/MongoDB/main.py
@@ -1,9 +1,11 @@
 import json
-
 from flask import Flask, request, jsonify
 from database import get_database
 from time import time
-from bson import json_util
-from bson.objectid import ObjectId
 
-app = Flask(__name__)
+database = get_database()
+collection = database["newCollection"]
+test = {
+    "test": "testing value"
+}
+collection.insert_one(test)
